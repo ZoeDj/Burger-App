8a6a1e73381017afd2d0b4492ac032ff49414038
ZoeDj <studio3fun@gmail.com>
2019-08-25 17:10:37 -0400

    finished

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,6 @@
 // Import MySQL connection.
 var connection = require("./connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -25,11 +20,10 @@ function objToSql(ob) {
     var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      
+
       arr.push(key + "=" + value);
     }
   }
@@ -69,14 +63,14 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  // An example of objColVals would be {name: cheeseburger, eaten: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
-    queryString += objToSql(objColVals);
+    queryString += objColVals;
     queryString += " WHERE ";
-    queryString += condition;
+    queryString += condition + ";";
 
     console.log(queryString);
     connection.query(queryString, function(err, result) {
